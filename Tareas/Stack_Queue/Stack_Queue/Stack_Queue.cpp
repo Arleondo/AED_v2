6e0ccba86272c9b7d5eeac109991f0bfccb4e334
Srgmail ;) <arnolsmith.tacca@ucsp.edu.pe>
2025-04-25 14:51:56 +0200

    Termine el L_queue y L_stack
    Nose causa me quede hasta la 1 ;-;

--- Tareas/Stack_Queue/Stack_Queue/Stack_Queue.cpp
+++ Tareas/Stack_Queue/Stack_Queue/Stack_Queue.cpp
@@ -7,9 +7,15 @@ int main() {
 
     Stack_a<int> stack(10);
     
-    for (int i = 10; i > 0; i) {
+    for (int i = 10; i > 0; i--) {
         stack.push(i);
     }
     stack.Print();
+    int a;
+    for (int i = 10; i > 0; i--) {
+        stack.pop(a);
+        cout << a << "\n";
+    }
+
 }
     
