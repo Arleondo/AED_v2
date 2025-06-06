a78dd5b2572f15d0500bb770f7c101e1d75b3279
Srgmail ;) <arnolsmith.tacca@ucsp.edu.pe>
2025-06-06 14:29:51 +0200

    Matrix sparse
    Actualizado

--- Clases/Matrix_Sparsa-02_06/Matrix_Spr.h
+++ Clases/Matrix_Sparsa-02_06/Matrix_Spr.h
@@ -2,6 +2,7 @@
 #define MATRIX_SPARSA_02_06_MATRIX_SPR_H
 
 #include <iostream>
+#include <map>
 
 struct node{
     int valor;
@@ -21,5 +22,52 @@ public:
 
 };
 
+
+class SparseMatrix {
+private:
+    int rows, cols;
+    std::map<std::pair<int, int>, int> data;
+
+public:
+    SparseMatrix(int r, int c) : rows(r), cols(c) {}
+
+    void set(int r, int c, int value) {
+        if (r >= rows || c >= cols || r < 0 || c < 0) {
+            std::cerr << "Índice fuera de rango.\n";
+            return;
+        }
+
+        if (value != 0)
+            data[{r, c}] = value;
+        else
+            data.erase({r, c}); // No almacenamos ceros
+    }
+
+    int get(int r, int c) const {
+        if (r >= rows || c >= cols || r < 0 || c < 0) {
+            std::cerr << "Índice fuera de rango.\n";
+            return 0;
+        }
+
+        auto it = data.find({r, c});
+        return (it != data.end()) ? it->second : 0;
+    }
+
+    void print() const {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                std::cout << get(i, j) << " ";
+            }
+            std::cout << "\n";
+        }
+    }
+
+    void printNonZeroElements() const {
+        std::cout << "Elementos no cero:\n";
+        for (const auto& [pos, val] : data) {
+            std::cout << "(" << pos.first << ", " << pos.second << ") = " << val << "\n";
+        }
+    }
+};
 
 #endif
--- Clases/Matrix_Sparsa-02_06/main.cpp
+++ Clases/Matrix_Sparsa-02_06/main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include "Matrix_Spr.h"
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SparseMatrix mat(4, 5);
+
+    mat.set(0, 1, 5);
+    mat.set(2, 3, 10);
+    mat.set(3, 4, -2);
+
+    cout << "Matriz completa:\n";
+    mat.print();
+
+    cout << "\nSolo elementos no cero:\n";
+    mat.printNonZeroElements();
+
     return 0;
 }
